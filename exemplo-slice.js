5246b226d54d694a80371ee58d916bfbd6e07ec2
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-23 10:05:31 -0300

    Adiciona o arquivo exemplo-includes na pasta metodo-Array

---
+++ exemplo-slice.js
@@ -0,0 +1,5 @@
+let playlist = ["Funk", "Hip-hop", "Pop", "Rock", "Jazz", "Reggae", "Classical", "Indie"];
+
+console.log(`Parte da playlist (idices 2 a 5): ${playlist.slice(2 , 6)}`); // [`Pop`, `Rock`, `Jazz`, `Reggae`]
+console.log(`Parte da playlist (idices 0 a 3): ${playlist.slice(0 , 4)}`); // [`Funk`, `Hip-hop`, `Pop`, `Rock`]
+console.log(`Parte da playlist (idice 3 a 4): ${playlist.slice(3 , 4)}`); 
