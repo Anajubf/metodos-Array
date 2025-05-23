6e7cf41dc4107b2fb94968500ddd59dcebcdfb22
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-23 08:47:46 -0300

    Adiciona exemplo-push na pasta de metodos-Array

---
+++ exemplo-push.js
@@ -0,0 +1,8 @@
+let playlist = ["Pop", "Rock", "Hip-hop"];
+
+console.log(`Antes do push: ${playlist}`);
+
+//Adicionando uma nova música à platlist
+playlist.push(`Trap`);
+
+console.log(`Despois do push: ${playlist}`);
