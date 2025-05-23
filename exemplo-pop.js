3e75738578e9879564cad7f57ac0d9d0672e7fe3
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-23 08:49:02 -0300

    Adiciona o arquivo exemplo-pop na pasta metodos-Array

---
+++ exemplo-pop.js
@@ -0,0 +1,12 @@
+let playlist = ["Pop" , "Rock" , "Hip-hop"];
+
+console.log(`Amtes do pop: ${playlist}`);
+
+//Removendo a última música da playlist
+let musicaRemovida = playlist.pop();
+
+console.log(`Depois do pop: ${playlist}`);
+console.log(`Música removida: ${musicaRemovida}`);
+
+let segundaMusicaRemovida = playlist.pop()
+console.log(`A segunda múisca removida: ${segundaMusicaRemovida}`);
