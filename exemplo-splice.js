bda828ef8d5d88daaed24be601584456dedf7504
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-23 10:50:16 -0300

    Modifica o arquivo exemplo-splice na pasta metodo-Array

--- exemplo-splice.js
+++ exemplo-splice.js
@@ -1,9 +1,16 @@
-let playlist = ["Funk", "Hip-hop", "Pop", "Rock", "Jazz"]
+let playlist = ["Funk", "Hip-hop", "Pop", "Rock", "Jazz", "Hino"];
 
 console.log(`Antes do splice: ${playlist}`);
 
 //Removendo 2 elementos a partir do índice 2
-let elementosRemovidos = playlist.splice(2, 2);
+//let elementosRemovidos = playlist.splice(2, 2);
 
-console.log(`Depois do splice: ${playlist}`);
-console.log(`Elementos removidos: ${elementosRemovidos}`);
+//console.log(`Depois do splice: ${playlist}`);
+//console.log(`Elementos removidos: ${elementosRemovidos}`);
+
+
+//Removendo 2 elementos a partir do indice 3
+let desafio = playlist.splice(3, 2);
+
+console.log(`Elementos removidos: ${desafio}`);
+console.log(`Playlist depois de remover os 2 índices: ${playlist}`);
