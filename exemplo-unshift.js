461f7181af8f6375c8840e57d228b880731709f8
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-23 09:02:30 -0300

    Adiciona o arquivo exemplo-unshift na pasta metodos-Array

---
+++ exemplo-unshift.js
@@ -0,0 +1,8 @@
+let playlist = ["Pop", "Rock", "Hip-Hop"];
+
+console.log(`Antes do unshift: ${playlist}`);
+
+//Adicionando uma nova música no ínicio da playlist
+playlist.unshift(`Mpb`)
+
+console.log(`Depois do unshift: ${playlist}`);
